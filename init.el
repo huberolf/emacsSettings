ba09517db169cb1accc9947461365d525763b4a1
PKAP <80753912+HamstyOnTour@users.noreply.github.com>
2023-11-09 14:47:40 +0100

    Create init.el
    added init.el for .emacs.d folder

---
+++ init.el
@@ -0,0 +1,19 @@
+;;Put this file in yout .emacs.d folder, for Windows this should be in AppData/Roaming/
+(setq straight-use-package-by-default t)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(use-package org
+  :straight t
+  :config
+  (org-babel-load-file "<PATH_TO_YOUR>/custominit.org"))
